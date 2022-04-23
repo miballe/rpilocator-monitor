80897dc4588043f6cb8237c07dd74c29ee58d46e
Miguel Ballesteros <git@miballe.dev>
2022-04-23 20:06:06 +0200

    Minor fixes and logging improvements

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
+from distutils.command import config
 import os
 import time
 import requests as r
 import pandas as pd
 import logging
+from sys import stdout
 
-logging.basicConfig(filename='/log/rpilocatormon.log', \
-                    filemode='w', \
-                    format='%(asctime)s - %(levelname)s - %(message)s',\
-                    level=logging.INFO)
+# Logger initialisation
+logger = logging.getLogger('rpimon')
+logger.setLevel(logging.INFO) # set logger level
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(funcName)s ¦ %(message)s")
+console_handler = logging.StreamHandler(stdout)
+console_handler.setFormatter(logFormatter)
+logger.addHandler(console_handler)
+file_handler = logging.FileHandler("/log/rpilocatormon.log", 'a+')
+file_handler.setFormatter(logFormatter)
+logger.addHandler(file_handler)
 
-logging.info('*** Script Start ***')
+logger.info('*** Script Start ***')
 
 # Constants
 iftt_webhook_base = 'https://maker.ifttt.com/trigger'
@@ -21,6 +29,7 @@ model_filter = os.getenv('MODEL_FILTER', 'RPi')
 ifttt_alert_name = os.getenv('IFTTT_ALERT_NAME', 'my_alert')
 ifttt_key = os.getenv('IFTTT_KEY', None)
 if ifttt_key is None:
+    logger.error('IFTTT_KEY environment variable not defined!')
     raise RuntimeError('IFTTT_KEY environment variable not defined!')
 file_test_mode = os.getenv('FILE_TEST_MODE', False)
 sleep_time_secs = 300
@@ -36,24 +45,24 @@ sleep_time_secs = int(os.getenv('SLEEP_TIME_SECS', sleep_time_secs))
 iftt_full_url = f'{iftt_webhook_base}/{ifttt_alert_name}/with/key/{ifttt_key}'
 
 def check_stock():
-    logging.info('** Main Loop Start **')
+    logger.info('** Main Loop Start **')
+    loop_n = 0
     while(True):
-        loop_n = 0
-        logging.info(f'* Loop {loop_n}')
+        logger.info(f'* Loop {loop_n}')
         if(file_test_mode):
-            rpidf = pd.read_xml('./sample.xml', xpath='.//item')
-            logging.debug(f'- From file {rpidf.shape[0]} records')
+            rpidf = pd.read_xml('app/sample.xml', xpath='.//item')
+            logger.debug(f'Found {rpidf.shape[0]} records in the sample.xml file')
         else:
             rpl_resp = r.get(rpiloc_url, headers={'accept': '*/*', 'user-agent': 'curl/7.68.0'})
             rpidf = pd.read_xml(rpl_resp.text, xpath='.//item')
-            logging.info(f'- From site {rpidf.shape[0]} records')
+            logger.info(f'Found {rpidf.shape[0]} records in the RPiLocator site')
         rpidf['ts'] = pd.to_datetime(rpidf['pubDate'], format='%a, %d %b %Y %H:%M:%S %Z')
         rpidf = rpidf[rpidf['description'].str.contains(model_filter)]
         ts_delta_ago = pd.to_datetime(pd.Timestamp('now', tz='GMT') - pd.Timedelta(event_delta_mins, 'minutes')).strftime('%Y-%m-%d %H:%M:%S')
         ts_delta_ago += ' GMT'
         rpidf = rpidf[rpidf['ts'] > ts_delta_ago]
 
-        logging.info(f'Found {rpidf.shape[0]} items for alerts')
+        logger.info(f'Found {rpidf.shape[0]} items to send alerts')
 
         if rpidf.shape[0] > 0:
             for index, row in rpidf.iterrows():
@@ -66,7 +75,7 @@ def check_stock():
                     'Content-Type': 'application/json'
                 }
                 ifttt_resp = r.post(iftt_full_url, json=msg, headers=hdrs)
-                logging.info(f'Notification sent to IFTTT with code {ifttt_resp.status_code}')
+                logger.info(f'Notification sent to IFTTT with code {ifttt_resp.status_code}')
         
         time.sleep(sleep_time_secs)
         loop_n += 1
@@ -74,4 +83,3 @@ def check_stock():
 
 if __name__ == "__main__":
     check_stock()
-
